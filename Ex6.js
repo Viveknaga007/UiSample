7330080856dfb2a7f1aef1d8f08c8e3871324c17
Viveknaga <nagavivek17@gmail.com>
2017-12-14 20:09:03 -0600

    added Ex 6,7,8.js

---
+++ Ex6.js
@@ -0,0 +1,37 @@
+function myfunction () {
+    var a = 4;
+    return a * a;    
+}
+
+////////
+
+var a = 4;
+function myfunction(){
+    return a * a;    
+}
+
+////////
+
+var counter = 0;
+
+function add() {
+    counter += 1;
+}
+
+add();
+add();
+add();
+
+/////////
+
+
+
+function add() {
+    var counter = 0;
+    counter += 1;
+}
+
+add();
+add();
+add();
+
