7a561ae4d78279841209c506d54b8b08d72f9a38
Viveknaga <nagavivek17@gmail.com>
2018-01-03 20:18:02 -0600

    Added my-app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -18,12 +18,16 @@ class App extends Component {
         
          <h1>My first react application!</h1>
         
-        <h1> Hello, {getGreeting(user)}  {tick()}  {A()}  </h1>
+        <p> Hello, {getGreeting(user)}  {tick()}  {A()}  </p>
 
         <img src={user.avatarUrl}></img>
 
         
-       
+        <Comment
+    date={comment.date}
+    text={comment.text}
+    author={comment.author}
+  />
    
         </div>
         
@@ -33,6 +37,48 @@ class App extends Component {
   }
   
 }
+
+
+function formatDate(date) {
+  return date.toLocaleDateString();
+}
+
+function Comment(props) {
+  return (
+    <div className="Comment">
+      <div className="UserInfo">
+        <img
+          className="Avatar"
+          src={props.author.avatarUrl}
+          alt={props.author.name}
+        />
+        <div className="UserInfo-name">
+          {props.author.name}
+        </div>
+      </div>
+      <div className="Comment-text">
+        {props.text}
+      </div>
+      <div className="Comment-date">
+        {formatDate(props.date)}
+      </div>
+    </div>
+  );
+}
+
+const comment = {
+  date: new Date(),
+  text:
+    'I hope you enjoy learning React!',
+  author: {
+    name: 'Hello Kitty',
+    avatarUrl:
+      'http://placekitten.com/g/64/64',
+  },
+};
+
+
+
 
 function Welcome(props) {
   return <h1>Hello, {props.name}</h1>;
